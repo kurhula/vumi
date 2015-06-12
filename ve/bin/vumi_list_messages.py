d07936a5fa93ba4d7d526edad88336698f1d2550
Musa Baloyi <musabaloyi@aims.ac.za>
2015-06-12 18:06:23 +0530

    Vumu Tutorial 1

---
+++ ve/bin/vumi_list_messages.py
@@ -0,0 +1,4 @@
+#!/Users/musabay/Projects/vumi/ve/bin/python
+# EASY-INSTALL-SCRIPT: 'vumi==0.5.20a0','vumi_list_messages.py'
+__requires__ = 'vumi==0.5.20a0'
+__import__('pkg_resources').run_script('vumi==0.5.20a0', 'vumi_list_messages.py')
